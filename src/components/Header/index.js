6fbc6a1ad2e09e57faa2ed3f694296a8825a9678
Bruno-Scharf <henriquescharf270@gmail.com>
2024-11-13 14:29:02 -0400

    bhss

---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import Link from "next/link"
+import styles from "./Header.module.css";
+
+export default function Header() {
+    return(
+        <header>
+            <nav>
+                <ul className={styles.lista}>
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/sobre">sobre</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
