4614460b7857b397be8b27dfb61fd41bae59ca34
Bruno-Scharf <henriquescharf270@gmail.com>
2024-11-18 10:10:17 -0400

    bhss
    Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,42 @@
+"use client"
 import Link from "next/link"
 import styles from "./Header.module.css";
+import { useState } from "react";
+import Image from "next/image";
+
 
 export default function Header() {
-    return(
-        <header>
-            <nav>
-                <ul className={styles.lista}>
-                    <li>
-                        <Link href="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link href="/sobre">sobre</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+  const [menuAberto, setMenuAberto] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuAberto(!menuAberto);
+  };
+
+  return (
+    <header className={styles.header}>
+      <nav aria-label="Menu de navegação">
+      <div className={styles.logo}>
+        <Image src="/public_img/liga-de-logotipo-de-volei_636967-253 (1).avif" alt="Logo da Aplicação" width={50} height={50} />
+      </div>
+
+
+        {/* Menu hamburguer para dispositivos móveis */}
+        <div className={styles.menuHamburguer} onClick={toggleMenu}>
+          <span className={menuAberto ? styles.open : ''}></span>
+          <span className={menuAberto ? styles.open : ''}></span>
+          <span className={menuAberto ? styles.open : ''}></span>
+        </div>
+
+        <ul className={`${styles.lista} ${menuAberto ? styles.ativo : ''}`}>
+          <li>
+            <Link href="/" aria-label="Ir para a página inicial"><h2>Inicio</h2></Link>
+          </li>
+          <li>
+            <Link href="/sobre" aria-label="Ir para a página Sobre"><h2>Sobre</h2></Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
