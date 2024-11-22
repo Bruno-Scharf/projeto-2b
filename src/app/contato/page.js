a5b97eb81648f49aa6bf1b0c89b3a9c81dc91533
Bruno-Scharf <henriquescharf270@gmail.com>
2024-11-22 14:50:32 -0400

    bhss

--- src/app/contato/page.js
+++ src/app/contato/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const contato = () => {
     return( <main>
-        <Image src="/public_img/liga-de-logotipo-de-volei_636967-253 (1).avif" alt="Logo da Aplicação" width={50} height={50} />
+        <h1>contato</h1>
     </main>
     )
     }    
